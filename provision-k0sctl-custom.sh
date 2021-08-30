f87c812867ea6a07206edb86933eec54cbb45db4
Rui Lopes <rgl@ruilopes.com>
2021-08-30 15:42:45 +0100

    upgrade to k0sctl v0.10.0-beta.3

--- provision-k0sctl-custom.sh
+++ provision-k0sctl-custom.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 source /vagrant/lib.sh
 
-# clone my repo which reverts f6074e98d22cd95684db9a480604cf881aeb3915
-git clone -b revert-f6074 https://github.com/rgl/k0sctl
-cd k0sctl
-go build
-ln -fs $PWD/k0sctl /usr/local/bin
-exit 0
-
 # install the version that better honours peerAddress.
 # see https://github.com/k0sproject/k0sctl
 # NB because we need https://github.com/k0sproject/k0sctl/pull/169 (https://github.com/k0sproject/k0sctl/issues/168)
 # NB because we need https://github.com/k0sproject/k0sctl/pull/181 (https://github.com/k0sproject/k0sctl/issues/179)
-go install github.com/k0sproject/k0sctl@00637405fbf20c98bc8cf66adb283e1fefb39477
+#go install github.com/k0sproject/k0sctl@00637405fbf20c98bc8cf66adb283e1fefb39477 # OK.
+#go install github.com/k0sproject/k0sctl@658cd398c053be6e227cabfc30dbcc78c13ae6a7 # broken. see https://github.com/k0sproject/k0sctl/issues/196
+go install github.com/k0sproject/k0sctl@590faf64f3fa81b5c6e37d37b9ee1c5a31c0066c # OK. v0.10.0-beta.3
 
 # move the release version out of the way.
 if [ -f /usr/local/bin/k0sctl ] && [ ! -L /usr/local/bin/k0sctl ]; then

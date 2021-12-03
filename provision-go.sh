b732f4727623ae2774af077830bb9510222ba717
Rui Lopes <rgl@ruilopes.com>
2021-12-03 05:55:29 +0000

    upgrade to go 1.17.4

--- provision-go.sh
+++ provision-go.sh
@@ -4,8 +4,8 @@ source /vagrant/lib.sh
 # install go.
 # see https://golang.org/dl/
 # see https://golang.org/doc/install
-artifact_url=https://golang.org/dl/go1.17.linux-amd64.tar.gz
-artifact_sha=6bf89fc4f5ad763871cf7eac80a2d594492de7a818303283f1366a7f6a30372d
+artifact_url=https://golang.org/dl/go1.17.4.linux-amd64.tar.gz
+artifact_sha=adab2483f644e2f8a10ae93122f0018cef525ca48d0b8764dae87cb5f4fd4206
 artifact_path="/tmp/$(basename $artifact_url)"
 wget -qO $artifact_path $artifact_url
 if [ "$(sha256sum $artifact_path | awk '{print $1}')" != "$artifact_sha" ]; then

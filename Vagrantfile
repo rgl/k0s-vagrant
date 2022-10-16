57fbc22c8a44a7a767f514bc7a7eb5777868bf65
Rui Lopes <rgl@ruilopes.com>
2022-10-16 11:51:15 +0100

    upgrade to helmfile 0.147.0

--- Vagrantfile
+++ Vagrantfile
@@ -18,7 +18,7 @@ CONFIG_K9S_VERSION = 'v0.26.6' # see https://github.com/derailed/k9s/releases
 CONFIG_KREW_VERSION = 'v0.4.3' # see https://github.com/kubernetes-sigs/krew/releases
 CONFIG_ETCDCTL_VERSION = 'v3.5.4' # see https://github.com/etcd-io/etcd/releases BUT make sure you use a version compatible with k0s go.mod.
 CONFIG_HELM_VERSION = 'v3.9.4' # see https://github.com/helm/helm/releases BUT make sure you use a version compatible with k0s go.mod.
-CONFIG_HELMFILE_VERSION = 'v0.144.0' # see https://github.com/roboll/helmfile/releases
+CONFIG_HELMFILE_VERSION = '0.147.0' # see https://github.com/helmfile/helmfile/releases
 
 hosts = """
 127.0.0.1	localhost
--- provision-helmfile.sh
+++ provision-helmfile.sh
@@ -4,21 +4,22 @@ set -euo pipefail
 #
 # deploy helmfile.
 
-helmfile_version="${1:-v0.144.0}"; shift || true
+helmfile_version="${1:-0.147.0}"; shift || true
 
 # install helmfile.
-# see https://github.com/roboll/helmfile#installation
+# see https://github.com/helmfile/helmfile#installation
 echo "installing helmfile $helmfile_version..."
 case `uname -m` in
     x86_64)
-        wget -qOhelmfile "https://github.com/roboll/helmfile/releases/download/$helmfile_version/helmfile_linux_amd64"
+        wget -qOhelmfile.tgz "https://github.com/helmfile/helmfile/releases/download/v$helmfile_version/helmfile_${helmfile_version}_linux_amd64.tar.gz"
         ;;
     armv7l)
-        wget -qOhelmfile "https://github.com/roboll/helmfile/releases/download/$helmfile_version/helmfile_linux_arm64"
+        wget -qOhelmfile.tgz "https://github.com/helmfile/helmfile/releases/download/v$helmfile_version/helmfile_${helmfile_version}_linux_arm64.tar.gz"
         ;;
 esac
+tar xf helmfile.tgz helmfile
 install helmfile /usr/local/bin
-rm helmfile
+rm helmfile.tgz
 
 # kick the tires.
 printf "#\n# helmfile version\n#\n"

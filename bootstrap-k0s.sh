62d276d500abb50d3b24f2edeccc6004acb6bd23
Rui Lopes <rgl@ruilopes.com>
2022-11-18 18:11:21 +0000

    upgrade to k0s 1.25.4+k0s.0

--- bootstrap-k0s.sh
+++ bootstrap-k0s.sh
@@ -16,8 +16,8 @@ bash /vagrant/provision-haproxy-config.sh \
   "$(jq -r '.nodes[] | select(.type == "controller") | .ipAddress' /vagrant/shared/config.json | tr '\n' ',' | sed -E 's/,$//g')"
 
 # generate the k0sctl.yaml configuration file.
-# see https://docs.k0sproject.io/v1.25.2+k0s.0/k0sctl-install/
-# see https://docs.k0sproject.io/v1.25.2+k0s.0/configuration/
+# see https://docs.k0sproject.io/v1.25.4+k0s.0/k0sctl-install/
+# see https://docs.k0sproject.io/v1.25.4+k0s.0/configuration/
 python3 <<'EOF'
 import json
 
@@ -74,7 +74,7 @@ def save_k0sctl_config():
                                     # see https://artifacthub.io/packages/helm/traefik/traefik
                                     # see https://github.com/traefik/traefik-helm-chart
                                     # see https://github.com/traefik/traefik-helm-chart/blob/master/traefik/values.yaml
-                                    # see https://docs.k0sproject.io/v1.25.2+k0s.0/examples/traefik-ingress/
+                                    # see https://docs.k0sproject.io/v1.25.4+k0s.0/examples/traefik-ingress/
                                     {
                                         'name': 'traefik',
                                         'chartname': 'traefik/traefik',
@@ -508,7 +508,7 @@ etcd-info endpoint status
 
 # show the nodes.
 # NB the controller nodes do not appear in this list.
-# see https://docs.k0sproject.io/v1.25.2+k0s.0/FAQ/#why-doesnt-kubectl-get-nodes-list-the-k0s-controllers
+# see https://docs.k0sproject.io/v1.25.4+k0s.0/FAQ/#why-doesnt-kubectl-get-nodes-list-the-k0s-controllers
 kubectl get nodes -o wide
 
 # add the custom registry to the default service account (in the default namespace).

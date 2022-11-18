2fef64b12c6bf9b361c6b9f45eea243a63bb9f5d
Rui Lopes <rgl@ruilopes.com>
2022-11-18 18:07:28 +0000

    upgrade to external-dns chart 6.12.0 (app version 0.13.1)

--- bootstrap-k0s.sh
+++ bootstrap-k0s.sh
@@ -143,7 +143,7 @@ def save_k0sctl_config():
                                     {
                                         'name': 'external-dns',
                                         'chartname': 'bitnami/external-dns',
-                                        'version': '6.10.2', # installs external-dns 0.12.2.
+                                        'version': '6.12.0', # installs external-dns 0.13.1.
                                         'namespace': 'cluster-external-dns',
                                         'values':
                                             f'''

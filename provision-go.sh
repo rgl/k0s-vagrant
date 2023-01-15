fbefe7ce051f934de0886685ee0c69f38a704393
Rui Lopes <rgl@ruilopes.com>
2023-01-15 18:09:17 +0000

    upgrade to go 1.19.5

--- provision-go.sh
+++ provision-go.sh
@@ -4,8 +4,8 @@ source /vagrant/lib.sh
 # install go.
 # see https://go.dev/dl/
 # see https://go.dev/doc/install
-artifact_url=https://go.dev/dl/go1.19.3.linux-amd64.tar.gz
-artifact_sha=74b9640724fd4e6bb0ed2a1bc44ae813a03f1e72a4c76253e2d5c015494430ba
+artifact_url=https://go.dev/dl/go1.19.5.linux-amd64.tar.gz
+artifact_sha=36519702ae2fd573c9869461990ae550c8c0d955cd28d2827a6b159fda81ff95
 artifact_path="/tmp/$(basename $artifact_url)"
 wget -qO $artifact_path $artifact_url
 if [ "$(sha256sum $artifact_path | awk '{print $1}')" != "$artifact_sha" ]; then

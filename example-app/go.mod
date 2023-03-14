7672b48a8b92b2cb0a31f23c6199c7d458b29fea
Rui Lopes <rgl@ruilopes.com>
2023-03-14 20:10:44 +0000

    upgrade to go 1.20.2

--- example-app/go.mod
+++ example-app/go.mod
@@ -1,3 +1,3 @@
 module github.com/rgl/example-app
 
-go 1.19
+go 1.20
--- provision-go.sh
+++ provision-go.sh
@@ -4,8 +4,8 @@ source /vagrant/lib.sh
 # install go.
 # see https://go.dev/dl/
 # see https://go.dev/doc/install
-artifact_url=https://go.dev/dl/go1.19.5.linux-amd64.tar.gz
-artifact_sha=36519702ae2fd573c9869461990ae550c8c0d955cd28d2827a6b159fda81ff95
+artifact_url=https://go.dev/dl/go1.20.2.linux-amd64.tar.gz
+artifact_sha=4eaea32f59cde4dc635fbc42161031d13e1c780b87097f4b4234cfce671f1768
 artifact_path="/tmp/$(basename $artifact_url)"
 wget -qO $artifact_path $artifact_url
 if [ "$(sha256sum $artifact_path | awk '{print $1}')" != "$artifact_sha" ]; then

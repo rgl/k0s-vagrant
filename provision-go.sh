0401c6c24cc907b68bf5cebb2b55da0493c14a7f
Rui Lopes <rgl@ruilopes.com>
2023-06-18 19:33:05 +0100

    upgrade to go 1.20.5

--- provision-go.sh
+++ provision-go.sh
@@ -4,14 +4,9 @@ source /vagrant/lib.sh
 # install go.
 # see https://go.dev/dl/
 # see https://go.dev/doc/install
-artifact_url=https://go.dev/dl/go1.20.2.linux-amd64.tar.gz
-artifact_sha=4eaea32f59cde4dc635fbc42161031d13e1c780b87097f4b4234cfce671f1768
+artifact_url=https://go.dev/dl/go1.20.5.linux-amd64.tar.gz
 artifact_path="/tmp/$(basename $artifact_url)"
 wget -qO $artifact_path $artifact_url
-if [ "$(sha256sum $artifact_path | awk '{print $1}')" != "$artifact_sha" ]; then
-    echo "downloaded $artifact_url failed the checksum verification"
-    exit 1
-fi
 rm -rf /usr/local/go
 install -d /usr/local/go
 tar xf $artifact_path -C /usr/local/go --strip-components 1

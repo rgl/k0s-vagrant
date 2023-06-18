643e08151b047bedc21c3043f64b0dcc101e234d
Rui Lopes <rgl@ruilopes.com>
2023-06-18 19:35:17 +0100

    upgrade to traefik chart 23.1.0 (app version 2.10.1)

--- bootstrap-k0s.sh
+++ bootstrap-k0s.sh
@@ -78,7 +78,7 @@ def save_k0sctl_config():
                                     {
                                         'name': 'traefik',
                                         'chartname': 'traefik/traefik',
-                                        'version': '21.2.0', # installs traefik 2.9.8.
+                                        'version': '23.1.0', # installs traefik 2.10.1.
                                         'namespace': 'cluster-traefik',
                                         'values':
                                             '''

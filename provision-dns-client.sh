81d380c5352e982f85eb511d9f9d1deef1e35279
Rui Lopes <rgl@ruilopes.com>
2023-06-19 21:13:35 +0100

    give up on systemd-resolved and just use resolv.conf to configure the machine to use the pandora dns server

--- provision-dns-client.sh
+++ provision-dns-client.sh
@@ -4,39 +4,12 @@ source /vagrant/lib.sh
 dns_server_ip_address="${1:-10.10.0.2}"; shift || true
 
 # change the dns resolver to use our dns server.
-if [ -d /etc/netplan ]; then
-  install -d /etc/systemd/resolved.conf.d
-  cat >/etc/systemd/resolved.conf.d/dns-servers.conf <<EOF
-[Resolve]
-DNS=$dns_server_ip_address
-Domains=~.
-EOF
-  systemctl restart systemd-resolved
-  cat >/etc/netplan/01-netcfg.yaml <<EOF
-# This file describes the network interfaces available on your system
-# For more information, see netplan(5).
-network:
-  version: 2
-  renderer: networkd
-  ethernets:
-    eth0:
-      dhcp4: yes
-      dhcp4-overrides:
-        use-dns: false
-EOF
-  netplan apply
-  # wait for the configuration to be applied by systemd-networkd.
-  # NB kubelet uses this file as --resolv-conf=/run/systemd/resolve/resolv.conf
-  while [ "$(awk '/^nameserver /{print $2}' /run/systemd/resolve/resolv.conf)" != "$dns_server_ip_address" ]; do
-    sleep 1
-  done
-  cat /run/systemd/resolve/resolv.conf
-else
-  cat >>/etc/dhcp/dhclient.conf <<EOF
-# make sure resolv.conf will always have our dns server.
-supersede domain-name-servers $dns_server_ip_address;
-EOF
-  cat >/etc/resolv.conf <<EOF
+# NB at some point in the ubuntu 22.04 lifecycle, using netplan
+#    dhcp4-overrides: use-dns: no stopped removing the dns server from eth0.
+#    so we now completly ignore systemd-resolved, and replace the
+#    /etc/resolv.conf symlink with a file.
+systemctl disable --now systemd-resolved
+rm -f /etc/resolv.conf 
+cat >/etc/resolv.conf <<EOF
 nameserver $dns_server_ip_address
 EOF
-fi
